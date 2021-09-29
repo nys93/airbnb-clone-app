e14321f22a83b5a6b0d82d90344e1bb657bbb581
Yousong <dbthd0220@naver.com>
2021-09-30 00:45:23 +0900

    fix layout

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import HomeImage from "../../assets/home-image.jpg";
 import "./Home.css";
 
 const Home: React.FC = (): JSX.Element => {
 	return (
 		<div className="home-wrapper">
 			<div className="home-top-wrapper">
+				<div className="home-top-text">
+					<h1>에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.</h1>
+				</div>
 				<div className="home-top-header">
 					<div className="home-top-menu"></div>
 					<div className="home-top-search"></div>
-					{/*<img src={HomeImage} className="home-image" alt="Home-img" />*/}
+					<div className="home-image-main"></div>
 				</div>
 			</div>
 			<div></div>
 			<div></div>
 			<div></div>
 			<div></div>
+			<div></div>
+			<div className="home-botttom-menu"></div>
+			<div className="home-bottom-footer"></div>
 		</div>
 	);
 };
