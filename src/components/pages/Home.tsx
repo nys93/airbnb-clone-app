c9183632545d172243ae6a60241a40a1bb9d6aa8
Yousong <dbthd0220@naver.com>
2021-10-27 23:34:42 +0900

    add main page css

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import "./Home.css";
+import Globe from "../../assets/globe.png";
+import Logo from "../../assets/airbnb-logo.png";
 
 const Home: React.FC = (): JSX.Element => {
 	return (
 		<div className="home-wrapper">
 			<div className="home-top-wrapper">
 				<div className="home-top-text">
-					<h1>에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.</h1>
+					<span>에어비앤비의 코로나19 대응 방안에 대한 최신 정보를 확인하세요.</span>
 				</div>
 				<div className="home-top-header">
-					<div className="home-top-menu"></div>
+					<div className="home-top-menu">
+						<div className="home-top-menu-left">
+							<a href="/">
+								<img src={Logo} className="airbnb-logo" />
+							</a>
+						</div>
+						<div className="home-top-menu-right">
+							<div>
+								<span>호스트 되기</span>
+							</div>
+							<div>
+								<a href="/">
+									<img src={Globe} className="globe-logo" />
+								</a>
+							</div>
+							<div></div>
+						</div>
+					</div>
 					<div className="home-top-search"></div>
 					<div className="home-image-main"></div>
 				</div>
